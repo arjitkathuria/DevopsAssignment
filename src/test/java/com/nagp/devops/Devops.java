c501ed498542d0d74283b55fb478cf35516b6980
Arjit Kathuria <arjit.kathuria@nagarro.com>
2021-01-12 11:58:24 +0530

    First Commit

---
+++ src/test/java/com/nagp/devops/Devops.java
@@ -0,0 +1,11 @@
+package com.nagp.devops;
+
+import org.testng.annotations.Test;
+
+public class Devops {
+
+    @Test
+    public void helloAssignment(){
+        System.out.println("Hello I am Devops Assignment ");
+    }
+}
